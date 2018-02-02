fe271cc67187ccbc3873e0d9ec98b4df5dbebb9c
alankaiko <alamdfs@hotmail.com>
2018-02-02 18:09:22 -0200

    ainda nas telas

--- src/main/java/br/com/projeto/gsv/tela/ClienteActionListener.java
+++ src/main/java/br/com/projeto/gsv/tela/ClienteActionListener.java
@@ -2,17 +2,28 @@ package br.com.projeto.gsv.tela;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import br.com.projeto.gsv.domain.Cliente;
+import br.com.projeto.gsv.domain.Contato;
+import br.com.projeto.gsv.domain.Endereco;
+import br.com.projeto.gsv.domain.SexoType;
+import br.com.projeto.gsv.domain.TipoPessoa;
 import br.com.projeto.gsv.service.CadastroClienteService;
 
 public class ClienteActionListener implements ActionListener{
 
-	private FrmIncluirClienteCPF clienteForm;
+	private FrmIncluirClienteCPF formulario;
 	private CadastroClienteService service;
+	Cliente cliente;
+	private Contato contato;
+	private Endereco endereco;
 	
-	public ClienteActionListener(FrmIncluirClienteCPF clienteForm) {
-		this.clienteForm = clienteForm;
+	
+	public ClienteActionListener(FrmIncluirClienteCPF formulario) {
+		this.formulario = formulario;
 		service = new CadastroClienteService();
 		AdicionarListener();
 	}
@@ -23,27 +34,78 @@ private void Inserir(){
 	
 	private void Salvar(){}
 	
-	
+	//Classe pega os botoes do formulario e atrela à esta classe controller aqui (propria classe ClienteActionListener)
 	public void AdicionarListener(){
-		clienteForm.getBTNovo().addActionListener(this);
-		clienteForm.getBTGravar().addActionListener(this);
-		clienteForm.getBTCancelar().addActionListener(this);
+		formulario.getBTNovo().addActionListener(this);
+		formulario.getBTGravar().addActionListener(this);
+		formulario.getBTCancelar().addActionListener(this);
 	}
 	
+	
+	
+	
+	
+	
+	
+	/*-----------------------------------------------------------------------------------------------------------------*/
+	/*-------------------CLASSES INTERNAS QUE CAPTURAM AS INFORMACOES E INSEREM NOS RESPECTIVOS OBJETOS ---------------*/
+	
+	//Classe que mapeia os objetos contato e endereco dentro do objeto cliente
+	private void AtrelarObjetos(){
+		this.cliente.getContato().add(this.contato);
+		this.cliente.getEndereco().add(this.endereco);
+		
+		this.endereco.setCliente(this.cliente);
+		this.contato.setCliente(this.cliente);
+	}	
+	
+	//Classe que pega informacoes do cliente e joga dentro do objeto Cliente
 	private Cliente FormToCliente(){
-		Cliente cliente = new Cliente();
+		cliente = new Cliente();
+		cliente.setNome(this.formulario.getTNome().getText());
+		cliente.setDocumento(this.formulario.getJCpf().getText());
+		cliente.setRg_insc(this.formulario.getTRg_insc().getText());
+		cliente.setDataCad(new Date());
+		cliente.setDataNasc(new Date());
+		cliente.setSexo(SexoType.MASCULINO);
+		cliente.setObservacao(this.formulario.getTObservacao().getText());
 		
-		if(!"".equals(clienteForm.getLId().getText())){
-			cliente.setId(Long.parseLong(clienteForm.getLId().getText()));
-		}
-
-		cliente.setNome(this.clienteForm.getTNome().getText());
-		
+		return cliente;
 	}
 	
+	//Classe que pega informacoes dos contatos e Joga dentro do objeto Contatos
+	private Contato FormToContato(){
+		contato = new Contato();
+		
+		contato.setEmail(this.formulario.getTEmail().getText());
+		contato.setCelular(this.formulario.getTCelular().getText());
+		contato.setTelefone(this.formulario.getTTelefone().getText());
+		
+		return contato;
+	}
+	
+	//Classe que pega infromacoes do Endereco e joga dentro do objeto Endereco
+	private Endereco FormToEndereco(){
+		endereco = new Endereco();
+		
+		endereco.setLogradouro(this.formulario.getTLogradouro().getText());
+		endereco.setComplemento(this.formulario.getTComplemento().getText());
+		endereco.setNumero(Double.parseDouble(this.formulario.getTNumero().getText()));
+		endereco.setBairro(this.formulario.getTBairro().getText());
+		endereco.setCidade(this.formulario.getTCidade().getText());
+		endereco.setEstado(this.formulario.getTCep().getText());
+		
+		return endereco;
+	}
 	
 
 	
+	
+	
+	/*-----------------------------------------------------------------------------------------------------------------*/
+	/*-------------------CLASSES QUE POSSUI AS ACOES DOS BOTOES ---------------*/
+	
+	//Classe que possui eventos dos botoes da TELA
 	public void actionPerformed(ActionEvent event) {
 		if(event.getActionCommand().equals("Inserir")){
 			
