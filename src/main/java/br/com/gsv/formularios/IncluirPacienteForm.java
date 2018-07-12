6f1b94158a770c3075ef53ee165a34f8e936e696
alankaiko <alamdfs@hotmail.com>
2018-07-12 18:04:01 -0300

    tirando as telas classic

--- src/main/java/br/com/gsv/formularios/IncluirPacienteForm.java
+++ src/main/java/br/com/gsv/formularios/IncluirPacienteForm.java
@@ -1,7 +1,12 @@
 package br.com.gsv.formularios;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
+import java.util.Vector;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
@@ -9,10 +14,13 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 import br.com.gsv.graficoClassic.IncluirPacienteClassic;
 import br.com.gsv.listeners.IncluirPacienteListener;
 import br.com.gsv.util.ConverteDadosUtil;
+import br.com.gsv.util.ListasUtil;
+import br.com.gsv.util.SomenteNumerosUtil;
 
 public class IncluirPacienteForm extends JDialog {
 	private IncluirPacienteListener listener;
@@ -26,48 +34,205 @@ public class IncluirPacienteForm extends JDialog {
 	private JFormattedTextField JCpf, JDataNasc, JDataCadastro, JCep;
 	private JButton BTGravar, BTCancelar;
 	private JComboBox ComboEstado, ComboConvenio;
-	private IncluirPacienteClassic listenerClassic;
 	private int xx,xy;
+	private SomenteNumerosUtil soNumeros;
 	
 	
 	public IncluirPacienteForm() {
-		CriaVariaveis();
-		InicializaCompomentes();
+		setTitle("Inserir Dados de Paciente");
+		setModal(true);
+		setResizable(false);
+		setBounds(100, 100, 650, 460);
+		
+		tela.setLayout(null);		
+		tela.add(panel, BorderLayout.CENTER);
+		
+		panel.setLayout(null);
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		OrganizaDados();
 		listener = new IncluirPacienteListener(this);
-		listenerClassic = new IncluirPacienteClassic(this);
 	}
 	
+	
+	public void OrganizaDados(){
+		LIndentif = new JLabel();
+		LIndentif.setText("DADOS PESSOAIS");
+		LIndentif.setBounds(15,10,300,20);
+		LIndentif.setFont(new Font("Calibri", Font.BOLD, 13));
+		LIndentif.setForeground(Color.black);
+		
+		
+			LId = new JLabel();
+			LId.setText("Código:");
+			LId.setBounds(65,30,50,20);
+			LId.setFont(new Font("Arial",Font.BOLD,11));
+				TId = new JTextField();
+				TId.setBounds(111,31,84,20);
+				TId.setBackground(new Color(255,255,204));
+				TId.setFont(new Font("Arial",Font.BOLD,10));
+				TId.setForeground(Color.black);
+				TId.setEditable(false);
+					
+			LDocumento = new JLabel();	 
+			LDocumento.setText("*CPF:");
+			LDocumento.setBounds(75,60,35,20);
+			LDocumento.setFont(new Font("Arial" , Font.BOLD,11));
+				JCpf= new JFormattedTextField(ConverteDadosUtil.FormataCPF());
+				JCpf.setBounds(111,62,173,19);
+			
+
+			LDataNasc = new JLabel();
+			LDataNasc.setText("Data Nasc.:");
+			LDataNasc.setBounds(50,121,60,20);
+			LDataNasc.setFont(new Font("Arial" , Font.BOLD,11));	
+				JDataNasc= new JFormattedTextField(ConverteDadosUtil.FormataData());
+				JDataNasc.setBounds(111,121,120,19);
+					
+			LRg = new JLabel();								
+			LRg.setText("*RG:");
+			LRg.setBounds(456,60,29,20);
+			LRg.setFont(new Font("Arial" , Font.BOLD,11));
+				TRg = new JTextField();
+				TRg.setBounds(489,61,120,19);
+				TRg.setFont(new Font("Arial",Font.BOLD,10));
+				TRg.addKeyListener(soNumeros);
+				
+			LNome = new JLabel();
+			LNome.setText("*Nome:");
+			LNome.setBounds(65,90,50,20);
+			LNome.setFont(new Font("Arial" , Font.BOLD,11));
+				TNome = new JTextField();
+				TNome.setBounds(111,91,500,19);
+				TNome.setFont(new Font("Arial",Font.BOLD,10));
+				
+			LDataCad = new JLabel();
+			LDataCad.setText("Data Cadastro:");
+			LDataCad.setBounds(439,30,84,20);
+			LDataCad.setFont(new Font("Arial" , Font.BOLD,11));
+				JDataCadastro= new JFormattedTextField(ConverteDadosUtil.FormataData());
+				JDataCadastro.setEditable(false);
+				JDataCadastro.setText(ConverteDadosUtil.RetornaDataAtual());
+				JDataCadastro.setBounds(525,30,84,19);
+				
+			LConvenio.setText("Convênio / Plano");
+			LConvenio.setBounds(290, 124, 220, 14);
+				ComboConvenio.setBounds(409, 121, 133, 20);
+				
+				LAdicionaConvenio.setText("Adicionar?");
+				LAdicionaConvenio.setForeground(Color.BLUE);
+				LAdicionaConvenio.setFont(new Font("Palatino Linotype", Font.PLAIN, 10));
+				LAdicionaConvenio.setBounds(552, 130, 123, 14);
+
+			LObservacao = new JLabel();
+			LObservacao.setText("Observação:");
+			LObservacao.setBounds(31,361,72,20);
+			LObservacao.setFont(new Font("Arial" , Font.BOLD,11));;
+				TObservacao = new JTextField();
+				TObservacao.setBounds(111,362,506,19);
+				TObservacao.setFont(new Font("Arial",Font.BOLD,10));
+				
+		LEndereco = new JLabel();
+		LEndereco.setText("ENDEREÇO");
+		LEndereco.setFont(new Font("Calibri", Font.BOLD, 13));
+		LEndereco.setBounds(10, 142, 64, 14);
+		
+			
+			
+			LLogradouro = new JLabel();
+			LLogradouro.setText("Logradouro: ");
+			LLogradouro.setFont(new Font("Arial", Font.BOLD, 11));
+			LLogradouro.setBounds(31, 167, 84, 14);
+				TLogradouro = new JTextField();
+				TLogradouro.setBounds(111, 164, 506, 20);
+				TLogradouro.setColumns(10);
+				
+			LComplemento = new JLabel();
+			LComplemento.setText("Complemento: ");
+			LComplemento.setFont(new Font("Arial", Font.BOLD, 11));
+			LComplemento.setBounds(15, 195, 100, 14);
+				TComplemento = new JTextField();
+				TComplemento.setColumns(10);
+				TComplemento.setBounds(111, 192, 506, 20);
+					
+			LNumero = new JLabel();
+			LNumero.setText("Numero:");
+			LNumero.setFont(new Font("Arial", Font.BOLD, 11));
+			LNumero.setBounds(57, 228, 58, 14);
+				TNumero = new JTextField();
+				TNumero.setBounds(111, 225, 106, 20);
+				TNumero.setColumns(10);
+				TNumero.addKeyListener(soNumeros);
+				
+			LBairro = new JLabel();
+			LBairro.setText("Bairro: ");
+			LBairro.setFont(new Font("Arial", Font.BOLD, 11));
+			LBairro.setBounds(363, 228, 46, 14);
+				TBairro = new JTextField();
+				TBairro.setBounds(419, 225, 192, 20);
+				TBairro.setColumns(10);
+				
+			LCidade = new JLabel();
+			LCidade.setText("Cidade: ");
+			LCidade.setFont(new Font("Arial", Font.BOLD, 11));
+			LCidade.setBounds(65, 253, 46, 14);
+				TCidade = new JTextField();
+				TCidade.setBounds(111, 250, 208, 20);
+				TCidade.setColumns(10);
+		
+			LEstado.setText("UF: ");
+			LEstado.setBounds(429, 253, 46, 14);
+				ComboEstado.setModel(new DefaultComboBoxModel(new Vector(ListasUtil.Estados())));
+				ComboEstado.setBounds(456, 250, 86, 20);
+				
+				
+			
+			LCep = new JLabel();
+			LCep.setText("CEP:");
+			LCep.setFont(new Font("Arial", Font.BOLD, 11));
+			LCep.setBounds(75, 284, 35, 14);
+				JCep = new JFormattedTextField(ConverteDadosUtil.FormataCep());
+				JCep.setBounds(111, 281, 86, 20);
+					
+				
+		LContato.setText("CONTATO");
+		LContato.setFont(new Font("Calibri", Font.BOLD, 13));
+		LContato.setBounds(15, 308, 72, 14);
+		
+			LEmail.setText("Email:");
+			LEmail.setFont(new Font("Arial", Font.BOLD, 11));
+			LEmail.setBounds(50, 325, 46, 14);
+				TEmail.setBounds(109, 322, 263, 20);
+				TEmail.setColumns(10);
+			
+			LCelular.setText("Celular:");
+			LCelular.setFont(new Font("Arial", Font.BOLD, 11));
+			LCelular.setBounds(404, 284, 46, 14);
+				TCelular.setBounds(456, 284, 155, 20);
+				TCelular.setColumns(10);
+			
+			LTelefone.setText("Telefone:");
+			LTelefone.setFont(new Font("Arial", Font.BOLD, 11));
+			LTelefone.setBounds(394, 325, 72, 14);
+				TTelefone.setBounds(456, 322, 155, 20);
+				TTelefone.setColumns(10);	
+					
+		
+		BTGravar.setText("Gravar");
+		BTGravar.setFont(new Font("Calibri", Font.PLAIN, 12));
+		BTGravar.setBounds(210, 398, 89, 23);
+		
+		BTCancelar.setText("Cancelar");
+		BTCancelar.setFont(new Font("Calibri", Font.PLAIN, 12));
+		BTCancelar.setBounds(315, 398, 89, 23);
+	}
 
 		
 	private void CriaVariaveis(){
-		TId = new JTextField();
-		TNome = new JTextField();
-		TRg = new JTextField();
-		TObservacao = new JTextField();
-		LId = new JLabel();
-		LNome = new JLabel();
-		LDocumento = new JLabel();
-		LRg = new JLabel();
-		LDataNasc = new JLabel();
-		LDataCad = new JLabel();
-		LObservacao = new JLabel();
-		LIndentif = new JLabel();
-		JDataCadastro= new JFormattedTextField(ConverteDadosUtil.FormataData());
-		JCpf= new JFormattedTextField(ConverteDadosUtil.FormataCPF());
-		JCep = new JFormattedTextField(ConverteDadosUtil.FormataCep());
-		JDataNasc= new JFormattedTextField(ConverteDadosUtil.FormataData());
-		LEndereco = new JLabel();
-		LLogradouro = new JLabel();
-		TLogradouro = new JTextField();
-		LComplemento = new JLabel();
-		TComplemento = new JTextField();
-		LNumero = new JLabel();
-		LBairro = new JLabel();
-		LCidade = new JLabel();
-		LCep = new JLabel();
-		TNumero = new JTextField();
-		TBairro = new JTextField();
-		TCidade = new JTextField();
+		
+		
+		
+		
+	
 		LContato = new JLabel();
 		LEmail = new JLabel();
 		LCelular = new JLabel();
